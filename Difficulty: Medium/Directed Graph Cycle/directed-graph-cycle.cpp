aecf1b1ebb94e45cb05b0f89ca6bc5c9b5098d27
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-07-18 22:44:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,59 +1,43 @@
 class Solution {
-public:
-
-    bool dfs(int node,
-             vector<int> adj[],
-             vector<bool> &visited,
-             vector<bool> &inRecursion) {
-
-        visited[node] = true;
-        inRecursion[node] = true;
-
-        for(int &neighbour : adj[node]) {
-
-            if(!visited[neighbour]) {
-
-                if(dfs(neighbour, adj, visited, inRecursion)) {
+  public:
+  
+    bool dfs(int node, vector<vector<int>>&adj, vector<int>&visited, vector<int>&pathVisited){
+        visited[node]=1;
+        pathVisited[node]=1;
+        
+        for(int neighbour:adj[node]){
+            if(!visited[neighbour]){
+                if(dfs(neighbour, adj, visited, pathVisited)==true){
                     return true;
                 }
-            }
-            else if(inRecursion[neighbour]) {
+            }else if(visited[neighbour]==true && pathVisited[neighbour]==true){
                 return true;
             }
         }
-
-        inRecursion[node] = false;
-
+        pathVisited[node]=0;
         return false;
     }
-
+  
     bool isCyclic(int V, vector<vector<int>> &edges) {
-
-        // adjacency list
-        vector<int> adj[V];
-
-        // edge list -> adjacency list
-        for(auto &edge : edges) {
-
-            int u = edge[0];
-            int v = edge[1];
-
+        // code here
+        vector<vector<int>>adj(V);
+        for(auto &edge:edges){
+            int u=edge[0];
+            int v=edge[1];
             adj[u].push_back(v);
         }
-
-        vector<bool> visited(V, false);
-        vector<bool> inRecursion(V, false);
-
-        for(int i = 0; i < V; i++) {
-
-            if(!visited[i]) {
-
-                if(dfs(i, adj, visited, inRecursion)) {
+        
+        vector<int>visited(V,0);
+        vector<int>pathVisited(V,0);
+        
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(dfs(i,adj,visited,pathVisited)==true){
                     return true;
                 }
             }
         }
-
         return false;
+        
     }
 };
