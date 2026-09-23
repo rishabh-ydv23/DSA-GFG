50c6e447b79694cfd2508fc74c4a97b390c98ee1
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-09-23 12:44:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove Duplicates from Unsorted/remove-duplicates-from-unsorted.java
+++ Difficulty: Basic/Remove Duplicates from Unsorted/remove-duplicates-from-unsorted.java
@@ -1,12 +1,12 @@
 class Solution {
     static ArrayList<Integer> removeDuplicate(int arr[]) {
-        // code here
-        LinkedHashSet<Integer> mp=new LinkedHashSet<>();
-        for(int el:arr){
+        LinkedHashSet<Integer> mp = new LinkedHashSet<>();
+
+        for(int el : arr) {
             mp.add(el);
         }
-        
-        ArrayList<Integer>li=new ArrayList<>(mp);
+
+        ArrayList<Integer> li = new ArrayList<>(mp);
         return li;
     }
 }
